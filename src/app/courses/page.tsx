118fa8001cfbc1be4fb3bdb5b587ee0c5ddf1014
TabiB <abendakfal07@gmail.com>
2026-05-11 22:29:59 +0100

    Delete src directory

--- src/app/courses/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getCategories, getLessons } from "@/app/actions/content";
-import CoursesClient from "./CoursesClient";
-
-export default async function CoursesPage() {
-  const categories = await getCategories("YEAR");
-  const lessons = await getLessons();
-
-  return <CoursesClient categories={categories} lessons={lessons} />;
-}
